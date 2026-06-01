0984ecd7159629a61509f0c877199437d2c301fe
Dhaval Doshi <dhavaldoshi2000@gmail.com>
2026-06-01 23:16:01 +0530

    feat: configure audio_service background playback and update MainActivity to AudioServiceActivity

--- android/app/src/main/kotlin/com/dharmaguide/dharma_guide/MainActivity.kt
+++ android/app/src/main/kotlin/com/dharmaguide/dharma_guide/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.dharmaguide.dharma_guide
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
